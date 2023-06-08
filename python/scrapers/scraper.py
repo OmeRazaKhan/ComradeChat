08412eb9c8af35f581af30f7894bd4dc6a652881
Dylan Munro <dylmunro@gmail.com>
2023-06-08 19:19:26 -0400

    Scraper obtains the total number of pages in the search portal

--- python/scrapers/scraper.py
+++ python/scrapers/scraper.py
@@ -40,6 +40,46 @@ class Scraper:
             return True
         return False
 
+    def _get_button_number(self, button):
+        """
+        Returns the number embedded in a number to navigate to a different page in the Open Data Portal.
+
+        Params:
+            button: The WebElement version of the button.
+
+        Returns:
+            (int): The number on the button.
+            (None): If no number could be found on the button.
+        """
+        button_html = button.text
+        button_number_regex = "^(\d+)\D"
+        button_string = re.findall(button_number_regex, button_html)[0]
+        return int(button_string)
+
+    def _get_num_search_pages(self, first_page_url: str) -> int:
+        """
+        Returns the total number of pages in the Open Data Portal.
+
+        Params:
+            first_page_url (str): The URL of the first search page results in the Open Data Portal.
+
+        Returns:
+            (int): The total number of webpages in the portal.
+
+        Raises:
+            Exception: If the button to the last page can not be located.
+        """
+        self._driver.get(constants.OPEN_DATA_PORTAL_URL)
+        try:
+            last_page_button = WebDriverWait(self._driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '/html/body/main/div[1]/div[3]/div[13]/div/ul/li[7]/a'))
+            )
+            number = self._get_button_number(last_page_button)
+            return number
+        except Exception as e:
+            raise e
+
     def get_all_dataset_urls(self, max_urls: int = None) -> list:
         """
         Returns a list of all URLs in the Open Data Portal which lead to a dataset.
@@ -47,7 +87,8 @@ class Scraper:
         Params:
             max_urls (int, default=None): The maximum amount of URLs to generate.
         """
-
+        total_pages = self._get_num_search_pages(
+            constants.OPEN_DATA_PORTAL_URL)
         self._driver.get(constants.OPEN_DATA_PORTAL_URL)
         try:
             root_div = WebDriverWait(self._driver, 10).until(
