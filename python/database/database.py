af1c91bff267f5514437daa9e4e8f34c08b4ad64
Dylan Munro <dylmunro@gmail.com>
2023-06-12 15:37:09 -0400

    retrieves metadata from database

--- python/database/database.py
+++ python/database/database.py
@@ -7,22 +7,44 @@ from python.database.metadata import Dataset, MetaData
 
 class Database:
 
-    def __init__(self, data_file_path: str):
-        """
-        Params:
-            data_file_path(str): The file path to the JSON file containing the scraped web data.
-        """
-        self._data = Parser().parse()
-        #data = list
-        ids = [current_data.get_id() for current_data in data]
-        ids_to_data = {
-            ids: self._data
-        }
-        self._data.get_csv_url()
+    def __init__(self, ids_to_metadata: dict):
+        self._ids_to_metadata = ids_to_metadata
 
-    @property
-    def data(self) -> Data:
+    def get_all_descriptions(self) -> dict:
         """
-        All data scraped from the web portal.
+        Returns:
+            (dict(int: string)): The ID of each metadata entry mapped to the description of its dataset.
         """
-        return self._data
+        ids_to_descriptions = dict()
+        for key in self._ids_to_metadata.keys():
+            metadata = self._ids_to_metadata.get(key)
+            description = metadata.description
+            ids_to_descriptions[key] = description
+        return ids_to_descriptions
+
+    def get_metadata(self, key) -> dict:
+        """
+        Returns all metadata for the specified key.
+        """
+        return self._ids_to_metadata.get(key)
+
+    def get_description(self, key) -> str:
+        """
+        Returns:
+            (str): The description of the dataset referred to by the given key.
+        """
+        return self._ids_to_metadata.get(key).description
+
+    def get_dataset_urls(self, key) -> str:
+        """
+        Returns:
+            (str): The URL to the dataset referred to by the given key.
+        """
+        return self._ids_to_metadata.get(key).dataset.url
+
+    def get_all_keys(self) -> list:
+        """
+        Returns:
+            (list): All keys in the database.
+        """
+        return self._ids_to_metadata.keys()
