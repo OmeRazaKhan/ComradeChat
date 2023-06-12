218f8666af9dc9ff41fdc88d654a4458bd41b7a0
Dylan Munro <dylmunro@gmail.com>
2023-06-12 10:04:42 -0400

    add dataset class

---
+++ python/parsers/supported_file_type.py
@@ -0,0 +1,15 @@
+"""
+All supported file types.
+"""
+from enum import Enum
+
+
+class SupportedFileType(Enum):
+    """
+    All supported file types for datasets.
+    """
+    CSV = "csv"
+    HTML = "html"
+    JSON = "json"
+    XLSX = "xlsx"
+    XML = "xml"
